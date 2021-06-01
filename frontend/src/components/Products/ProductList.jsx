a87747f70e93c2e965eda71552fc021158bf0dc4
Jacqueline <jlee4332@gmail.com>
2021-06-01 13:58:05 -0400

    add product details

--- frontend/src/components/Products/ProductList.jsx
+++ frontend/src/components/Products/ProductList.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ProductList = ( {product} ) => {
   return (
     <div className="card">
-      <img className="product-image" src={product.image} alt={product.name}/>
+      <img className="small-image" src={product.image} alt={product.name}/>
       <div>{product.name}</div>
       <div>${product.price}</div>
     </div>
--- frontend/src/components/Products/ProductScreen.jsx
+++ frontend/src/components/Products/ProductScreen.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import data from '../../data';
 
-const ProductScreen = () => {
+const ProductScreen = (props) => {
+  console.log("PROPS!", props);
+  const product = data.products.find( e => e._id === props.match.params.id);
+  if (!product) {
+    return <div> Product Not Found</div>;
+  }
+
   return (
-    <div>
-      hello
+    <div className="product-container">
+      <div >
+        <img className="medium-image" src={product.image} alt={product.name}></img>
+      </div>
+      <section className="product-details">
+        <div>{product.name}</div>
+        <div>${product.price}</div>
+        <div>{product.description}</div>
+        <div className="quantity-input">
+          <div>Quantity:</div>
+          <input size="4" max="9999" min="1" value="1" type="number" step="1"/>
+        </div>
+        <div>
+          <button className="add-to-cart">Add to Cart</button>
+        </div>
+      </section>
     </div>
   )
 }
