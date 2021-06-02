d02ee5f55128932820c82094f1b9f0463db56392
Jacqueline <jlee4332@gmail.com>
2021-06-02 15:57:47 -0400

    add backend

---
+++ backend/server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+const app = express()
+const port = 8000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.get('./api/products', (req,res) => {
+  res.send(data.products);
+})
