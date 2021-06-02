26b65f92c1879e88bb224b5dbf39a39345357625
Jacqueline <jlee4332@gmail.com>
2021-06-02 16:30:15 -0400

    fetch products from backend

--- frontend/src/components/Products/Products.jsx
+++ frontend/src/components/Products/Products.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import ProductList from './ProductList'
-import data from '../../data'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import ProductList from './ProductList';
+// import data from '../../data';
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data)
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="products-container">
-      {data.products.map((product) => {
+      {products.map((product) => {
         return <ProductList key={product._id} product={product}/>
       })};
     </div>
