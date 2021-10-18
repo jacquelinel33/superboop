451c7b4e34c859ccd2145a0e1b300f7bc85c7891
Jacqueline <jlee4332@gmail.com>
2021-10-18 14:39:16 -0700

    add signing post

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -2,6 +2,7 @@ import express from 'express';
 import User from '../models/userModel.js';
 import data from '../server/data.js';
 import expressAsyncHandler from 'express-async-handler';
+import bcrypt from 'bcryptjs';
 
 //allows routes in this file seperate from server/index.js
 const userRouter = express.Router();
@@ -12,5 +13,23 @@ userRouter.get('/seed', expressAsyncHandler( async(req, res) => {
   const createdUser = await User.insertMany(data.users);
   res.send({ createdUser });
 }));
+
+userRouter.post('/signin', expressAsyncHandler(async(req,res)=> {
+  const user = await User.findOne({email: req.body.email});
+  if(user) {
+    if(bcrypt.compareSync(req.body.password, user.password) {
+      res.send({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user),
+      });
+      return;
+    }
+  }
+  res.status(401).send({message: 'Invalid login'})  
+ })
+);
 
 export default userRouter;
