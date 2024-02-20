0cc9018744b71634d44ecc392441ffac41fdba86
Jacqueline Lee <jlee4332@gmail.com>
2024-02-19 17:34:22 -0800

    refactor createStore

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,33 +1,66 @@
-import { applyMiddleware, combineReducers, compose, createStore } from "redux";
-import thunk from 'redux-thunk';
-// import { cartReducer } from "./reducers/cartReducers";
-import { productDetailsReducer, productListReducer } from "./reducers/productReducers";
-import { cartSlice } from "./reducers/cartSlice";
+// import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+// import thunk from "redux-thunk";
+// // import { cartReducer } from "./reducers/cartReducers";
+// import {
+//   productDetailsReducer,
+//   productListReducer,
+// } from "./reducers/productReducers";
+// import { cartSlice } from "./reducers/cartSlice";
 
-//store holds global state
+// //store holds global state
 
-//get items from local storage, parse string into array or return empty array
-const initalState = {
-  cart:{
-    cartItems: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems'))
-    : [],
-  }
+// //get items from local storage, parse string into array or return empty array
+// const initalState = {
+//   cart: {
+//     cartItems: localStorage.getItem("cartItems")
+//       ? JSON.parse(localStorage.getItem("cartItems"))
+//       : [],
+//   },
+// };
+
+// const reducer = combineReducers({
+//   productList: productListReducer,
+//   productDetails: productDetailsReducer,
+//   // cart: cartReducer,
+//   //cart reducer uses createSlice() to create reducers
+//   cart: cartSlice.reducer,
+// });
+
+// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// const store = createStore(
+//   reducer,
+//   initalState,
+//   composeEnhancer(applyMiddleware(thunk))
+// );
+
+// export default store;
+
+import { configureStore } from '@reduxjs/toolkit';
+import { cartSlice } from './reducers/cartSlice';
+import {
+  productDetailsReducer,
+  productListReducer,
+} from "./reducers/productReducers";
+
+
+const preloadedState = {
+  cart: {
+    cartItems: localStorage.getItem('cartItems') 
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
+  },
 };
 
-const reducer = combineReducers({
-  productList: productListReducer,
-  productDetails: productDetailsReducer,
-  // cart: cartReducer,
-  //cart reducer uses createSlice() to create reducers
-  cart: cartSlice.reducer
-})
+const store = configureStore({
+  reducer: {
+    productList: productListReducer,
+    productDetails: productDetailsReducer,
+    cart: cartSlice.reducer,
+  },
+  preloadedState,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  devTools: process.env.NODE_ENV !== 'production', // Automatically use DevTools in non-production environments
+});
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  reducer, 
-  initalState, 
-  composeEnhancer(applyMiddleware(thunk))
-);
-
-export default store; 
+export default store;
